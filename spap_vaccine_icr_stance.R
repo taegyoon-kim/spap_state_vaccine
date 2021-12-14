ec309a27868b914599df223b9451ab315701f779
taegyoon-kim <57738014+taegyoon-kim@users.noreply.github.com>
2021-12-14 17:54:27 +0900

    Add files via upload

--- spap_vaccine_icr_stance.R
+++ spap_vaccine_icr_stance.R
@@ -1,6 +1,6 @@
 ###### author details: Taegyoon Kim, taegyoon@psu.edu
 ###### purpose: This script is for computing ICR scores for anti-vax hand-labeling.
-###### note: labels are from two independent coders
+###### note: labels are from two independent coders (-1: anti, 0: neutral, 1: pro, 9: unrelated to vaccine)
 ###### last edit: 14 Dec 2021
 
 
@@ -13,7 +13,7 @@ lapply(packages, library, character.only = TRUE)
 # read data -------------------------
 
 path <- '/Users/taegyoon/Google Drive/spap_state/spap_state_vaccine/classification/'
-df_stance_label <- read.csv(paste0(path, 'state_twitter_vaccine_hand_label_9200_19_april.csv'))
+df_stance_label <- read.csv(paste0(path, 'spap_vaccine_icr_stance.csv'))
 df_stance_label <- df_stance_label[which(df_stance_label$stance_final != 9), ]
 
 
